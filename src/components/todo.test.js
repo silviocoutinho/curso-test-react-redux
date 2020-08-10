1595b021bda760f8041b4750130fe62bbd70927d
Silvio Coutinho <silviofc79@gmail.com>
2020-08-10 16:57:59 -0300

    Refactor component

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -14,32 +14,29 @@ describe('<Todo /> component Unit Tests', () => {
         completed: false,
         text: 'do something'
     }
+    let component;
+    beforeEach(() => {
+        component = shallow(<Todo {...props} />);
+    })
+
     it('should render 1 <Todo /> component', ()=>{
-        const component = shallow(<Todo {...props} />);
         expect(component).toHaveLength(1);
         expect(component.find('li')).toHaveLength(1);
     })
 
-    it('should render props correctly', ()=>{
-        const component = shallow(<Todo {...props} />);
-        //console.log(component.props());
+    it('should render props correctly', ()=>{        
         expect(component.props().children).toEqual('do something');
-      
     })
 
-    it('should set props correctly', ()=>{
-        const component = shallow(<Todo {...props} />);
+    it('should set props correctly', ()=>{       
         component.setProps({ text: 'changed text'})
         expect(component.props().children).toEqual('changed text');
       
     })
 
     it('should call onClick handler when Todo component is clicked', ()=>{
-        const component = shallow(<Todo {...props} />);
         component.simulate('click');
         expect(mockFn).toHaveBeenCalledTimes(1);
-        
-      
     })
 })
 
